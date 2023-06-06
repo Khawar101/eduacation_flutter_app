40acb333f25509be0f01ec4fe05226beba2fa870
mudassir060 <mudassirmukhtar4@gmail.com>
2023-06-06 15:56:00 +0500

    notification widget

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
               bodyColor: Colors.black,
             ),
       ),
-      initialRoute: Routes.buttomBarView,
+      initialRoute: Routes.notificationView,
       onGenerateRoute: StackedRouter().onGenerateRoute,
       navigatorKey: StackedService.navigatorKey,
       navigatorObservers: [
