0501602547f843ab85e1a5cd85ce156dd8abe15c
mudassir060 <mudassirmukhtar4@gmail.com>
2023-06-06 16:00:21 +0500

    call widgets

--- lib/ui/views/notification/notification_view.dart
+++ lib/ui/views/notification/notification_view.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked/stacked.dart';
-
-import '../../widgets/app_utils.dart';
 import 'notification_viewmodel.dart';
+import 'widget.dart/messages.dart';
 import 'widget.dart/notification.dart';
 
 class NotificationView extends StackedView<NotificationViewModel> {
@@ -107,138 +106,11 @@ class NotificationView extends StackedView<NotificationViewModel> {
                       // left: MediaQuery.of(context).size.width * 0.06,
                       // right: MediaQuery.of(context).size.width * 0.06,
                       bottom: MediaQuery.of(context).size.height * 0.03),
-                  child: SizedBox(
+                  child: const SizedBox(
                     child: TabBarView(
                       //      physics: const NeverScrollableScrollPhysics(),
                       //   controller: _tabController,
-                      children: [
-                        MediaQuery.removePadding(
-                          context: context,
-                          removeTop: true,
-                          child: GestureDetector(
-                            onTap: () {
-                              // Navigator.push(
-                              //     context,
-                              //     MaterialPageRoute(
-                              //         builder: (_) => const ChatPageScreen()));
-                            },
-                            child: ListView.builder(
-                              itemCount: 7,
-                              itemBuilder: (BuildContext context, index) {
-                                return Padding(
-                                  padding: EdgeInsets.only(
-                                      bottom:
-                                          MediaQuery.of(context).size.height *
-                                              0.02),
-                                  child: Container(
-                                    // height: MediaQuery.of(context).size.height,
-                                    width: double.infinity,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      color: const Color(0xff4873a6)
-                                          .withOpacity(0.7),
-                                    ),
-                                    child: Padding(
-                                      padding: EdgeInsets.symmetric(
-                                          horizontal: MediaQuery.of(context)
-                                                  .size
-                                                  .width *
-                                              0.05,
-                                          vertical: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                              0.02),
-                                      child: Column(
-                                        children: [
-                                          Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.spaceBetween,
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.end,
-                                            children: [
-                                              Row(
-                                                crossAxisAlignment:
-                                                    CrossAxisAlignment.end,
-                                                children: [
-                                                  Container(
-                                                    height: 50,
-                                                    width: 50,
-                                                    decoration: BoxDecoration(
-                                                      color: Colors.black,
-                                                      borderRadius:
-                                                          BorderRadius.circular(
-                                                              10),
-                                                    ),
-                                                    child: ClipRRect(
-                                                        borderRadius:
-                                                            BorderRadius
-                                                                .circular(10),
-                                                        child: Image.asset(
-                                                            'assets/images/download.jpeg')),
-                                                  ),
-                                                  const SizedBox(width: 7),
-                                                  Column(
-                                                    crossAxisAlignment:
-                                                        CrossAxisAlignment
-                                                            .start,
-                                                    children: [
-                                                      Text(
-                                                        "Daniel Lawson",
-                                                        style: GoogleFonts
-                                                            .ibmPlexSans(
-                                                          color: Colors.white,
-                                                        ),
-                                                      ),
-                                                      const SizedBox(height: 8),
-                                                      Text(
-                                                        "Online",
-                                                        style: GoogleFonts
-                                                            .ibmPlexSans(
-                                                          fontSize: 12,
-                                                          color: Colors.white,
-                                                        ),
-                                                      ),
-                                                    ],
-                                                  )
-                                                ],
-                                              ),
-                                              Text(
-                                                "04:32 pm",
-                                                style: GoogleFonts.ibmPlexSans(
-                                                  color: Colors.white,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                          const SizedBox(height: 8),
-                                          const SmallText(
-                                              text:
-                                                  "When dealing with technical commands that use a keyboard (for example, ",
-                                              color: Colors.white),
-                                          const SizedBox(height: 15),
-                                          SizedBox(
-                                            height: 160,
-                                            width: double.infinity,
-                                            child: ClipRRect(
-                                              borderRadius:
-                                                  BorderRadius.circular(20),
-                                              child: Image.asset(
-                                                  'assets/images/download (1).jpeg',
-                                                  fit: BoxFit.fitWidth),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                        ),
-                        //Notification bar
-                      
-                    NotificationWidget()  ],
+                      children: [MessagesWidget(), NotificationWidget()],
                     ),
                   ),
                 ),
@@ -248,7 +120,6 @@ class NotificationView extends StackedView<NotificationViewModel> {
         ),
       ),
     );
- 
   }
 
   @override
--- lib/ui/views/notification/widget.dart/notification.dart
+++ lib/ui/views/notification/widget.dart/notification.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked/stacked.dart';
-
-import '../../../widgets/app_utils.dart';
 import '../notification_viewmodel.dart';
 
 class NotificationWidget extends StackedView<NotificationViewModel> {
