6d19ac9ae1c963afad1da1c467fd27f77c9e21de
mudassir060 <mudassirmukhtar4@gmail.com>
2023-06-08 18:12:25 +0500

    chats

--- lib/ui/views/buttom_bar/chat/chats/chats_viewmodel.dart
+++ lib/ui/views/buttom_bar/chat/chats/chats_viewmodel.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:education/app/app.router.dart';
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
@@ -10,4 +12,31 @@ class ChatsViewModel extends BaseViewModel {
   navigateinbox() {
     _navigationService.navigateToInboxView();
   }
+  // final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance
+  //   .collection('users')
+  //   // .orderBy('index', descending: true)
+  //   // .where('index', isGreaterThan: '3')
+  //   // .limitToLast(2)
+  //   .snapshots();
+
+  //  StreamBuilder<QuerySnapshot>(
+  //     stream: _usersStream,
+  //     builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+  //       if (snapshot.hasError) {
+  //         return Text('Something went wrong');
+  //       }
+
+  //       if (snapshot.connectionState == ConnectionState.waiting) {
+  //         return Center(
+  //           child: LoadingFadingLine.circle(),
+  //         );
+  //       }
+
+  //       return ListView(
+  //         children: snapshot.data!.docs.map((DocumentSnapshot document) {
+  //           Map<String, dynamic> data =
+  //               document.data()! as Map<String, dynamic>;
+  //           return Text("data");
+  //         }).toList(),
+  //       );
 }
