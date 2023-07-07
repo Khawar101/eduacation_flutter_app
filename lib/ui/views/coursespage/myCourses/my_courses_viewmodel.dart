be3dc45a9c09d9cbcdd1c994df2f7eda8ed08212
mudassir060 <mudassirmukhtar4@gmail.com>
2023-07-07 13:15:05 +0500

    update grid view

--- lib/ui/views/coursespage/myCourses/my_courses_viewmodel.dart
+++ lib/ui/views/coursespage/myCourses/my_courses_viewmodel.dart
@@ -28,115 +28,117 @@ class MyCoursesViewModel extends BaseViewModel {
     // _navigationService.navigateToCoursedetailView();
     notifyListeners();
   }
-  
-  Widget coursesBuilder() {
-    return StreamBuilder<List<CoursesModel>>(
-      stream: coursesService.coursesStream(),
-      builder:
-          (BuildContext context, AsyncSnapshot<List<CoursesModel>> snapshot) {
-        if (snapshot.hasError) {
-          return const Text('Something went wrong');
-        }
 
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Loading(100);
-        }
-        return GridView.builder(
-          shrinkWrap: true,
-          itemCount: snapshot.data!.length,
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 300,
-            mainAxisExtent: 250,
-            crossAxisSpacing: 8,
-            mainAxisSpacing: 8,
-          ),
-          itemBuilder: (BuildContext context, int index) {
-            var data = snapshot.data![index];
-            return GestureDetector(
-              onTap: () {
-                // checkSubscripNavigate(data);
-              },
-              child: Card(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Image.network(
-                      data.coverPic.toString(),
-                      fit: BoxFit.cover,
-                      height: 130,
-                      width: 300,
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Stack(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                left: 8.0,
-                                top: 8.0,
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    data.title.toString(),
-                                    style: const TextStyle(
-                                        overflow: TextOverflow.ellipsis,
-                                        fontWeight: FontWeight.bold),
-                                    maxLines: 1,
-                                  ),
-                                  RatingBar.builder(
-                                    wrapAlignment: WrapAlignment.start,
-                                    initialRating: data.rating ?? 5.0,
-                                    minRating: 1,
-                                    direction: Axis.horizontal,
-                                    allowHalfRating: true,
-                                    itemCount: 5,
-                                    itemSize: 15,
-                                    ignoreGestures: true,
-                                    itemBuilder: (context, _) => const Icon(
-                                      Icons.star,
-                                      color: Colors.yellow,
+  Widget coursesBuilder() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: StreamBuilder<List<CoursesModel>>(
+        stream: coursesService.coursesStream(),
+        builder:
+            (BuildContext context, AsyncSnapshot<List<CoursesModel>> snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Loading(100);
+          }
+          return GridView.builder(
+            shrinkWrap: true,
+            itemCount: snapshot.data!.length,
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 300,
+              mainAxisExtent: 250,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+            ),
+            itemBuilder: (BuildContext context, int index) {
+              var data = snapshot.data![index];
+              return GestureDetector(
+                onTap: () {
+                  // checkSubscripNavigate(data);
+                },
+                child: Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Image.network(
+                        data.coverPic.toString(),
+                        fit: BoxFit.cover,
+                        height: 130,
+                        width: 300,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Stack(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 8.0,
+                                  top: 8.0,
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      data.title.toString(),
+                                      style: const TextStyle(
+                                          overflow: TextOverflow.ellipsis,
+                                          fontWeight: FontWeight.bold),
+                                      maxLines: 1,
                                     ),
-                                    onRatingUpdate: (rating) {
-                                      //print(rating);
-                                    },
-                                  ),
-                                  CustomText(
-                                      text: "\$${data.price.toString()}",
-                                      size: 17,
-                                      fontWeight: FontWeight.bold,
-                                      color: Colors.black),
-                                ],
+                                    RatingBar.builder(
+                                      wrapAlignment: WrapAlignment.start,
+                                      initialRating: data.rating ?? 5.0,
+                                      minRating: 1,
+                                      direction: Axis.horizontal,
+                                      allowHalfRating: true,
+                                      itemCount: 5,
+                                      itemSize: 15,
+                                      ignoreGestures: true,
+                                      itemBuilder: (context, _) => const Icon(
+                                        Icons.star,
+                                        color: Colors.yellow,
+                                      ),
+                                      onRatingUpdate: (rating) {
+                                        //print(rating);
+                                      },
+                                    ),
+                                    CustomText(
+                                        text: "\$${data.price.toString()}",
+                                        size: 17,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black),
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 8, vertical: 4),
-                          child: Text(
-                            data.description.toString(),
-                            style: const TextStyle(
-                                color: Colors.black45,
-                                overflow: TextOverflow.ellipsis,
-                                fontWeight: FontWeight.bold),
-                            maxLines: 2,
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 4),
+                            child: Text(
+                              data.description.toString(),
+                              style: const TextStyle(
+                                  color: Colors.black45,
+                                  overflow: TextOverflow.ellipsis,
+                                  fontWeight: FontWeight.bold),
+                              maxLines: 2,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
-        );
-      },
+              );
+            },
+          );
+        },
+      ),
     );
   }
-
 }
