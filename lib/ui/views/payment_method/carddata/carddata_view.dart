6b6ede2f5554a12ea70088877c60b2906f5b8f80
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-07-23 16:25:41 +0500

    education app

--- lib/ui/views/payment_method/carddata/carddata_view.dart
+++ lib/ui/views/payment_method/carddata/carddata_view.dart
@@ -2,7 +2,6 @@ import 'package:education/ui/views/payment_method/carddata/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:stacked/stacked.dart';
-
 import '../../../widgets/app_utils.dart';
 import 'carddata_viewmodel.dart';
 
