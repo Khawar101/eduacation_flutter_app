9674f2bb0dc6ab147e1479b0222eb3fad1679c82
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-07-27 10:18:28 +0500

    formated

--- lib/ui/views/buttom_bar/student_information/student_information_view.dart
+++ lib/ui/views/buttom_bar/student_information/student_information_view.dart
@@ -1,8 +1,6 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import '../../../widgets/app_utils.dart';
 import '../../drawer/drawer_view.dart';
 import 'widgets/getTabwidget.dart';
