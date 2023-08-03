1072ea27e9d934bc89e3dc5ab3c1394c5c89c015
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-03 10:07:15 +0500

    fo

--- lib/ui/views/coursespage/favouritesub/widgets/favoriteCourseCard.dart
+++ lib/ui/views/coursespage/favouritesub/widgets/favoriteCourseCard.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: file_names
 import 'package:education/services/Model/CoursesModel.dart';
 import 'package:education/ui/widgets/app_utils.dart';
 import 'package:flutter/material.dart';
