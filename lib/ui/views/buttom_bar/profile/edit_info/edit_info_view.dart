36c6a22ccab18f0bbc82e4fa7a971c9293c33752
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-08 10:09:13 +0500

    sign_up services

--- lib/ui/views/buttom_bar/profile/edit_info/edit_info_view.dart
+++ lib/ui/views/buttom_bar/profile/edit_info/edit_info_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked/stacked.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
-
 import '../../../../widgets/app_utils.dart';
 import '../../../../widgets/custom_text_form_field.dart';
 import 'edit_info_viewmodel.dart';
