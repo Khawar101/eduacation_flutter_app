56797eb97b30f478c539c790786292a737e818cf
Khawar Ayoub <124525015+Khawar101@users.noreply.github.com>
2023-08-09 13:34:18 +0500

    messaging done

--- lib/ui/views/buttom_bar/chat/inbox/MessageBubble.dart
+++ lib/ui/views/buttom_bar/chat/inbox/MessageBubble.dart
@@ -7,31 +7,36 @@ class MessageBubble extends StatelessWidget {
   const MessageBubble({super.key, required this.isMe, required this.message});
 
   @override
-  
   Widget build(BuildContext context) {
     return Row(
+      mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
         Container(
           decoration: BoxDecoration(
             color: isMe ? Colors.blue : Colors.grey[300],
             borderRadius: BorderRadius.only(
-              topLeft: const Radius.circular(12.0),
-              topRight: const Radius.circular(12.0),
+              topLeft: const Radius.circular(10.0),
+              topRight: const Radius.circular(10.0),
               bottomLeft: isMe
-                  ? const Radius.circular(12.0)
+                  ? const Radius.circular(10.0)
                   : const Radius.circular(0.0),
               bottomRight: isMe
                   ? const Radius.circular(0.0)
-                  : const Radius.circular(12.0),
+                  : const Radius.circular(10.0),
             ),
           ),
-          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 16.0),
-          margin: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-          child: Text(
-            message,
-            style: TextStyle(
-              color: isMe ? Colors.white : Colors.black,
+          padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 12.0),
+          margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 8.0),
+          child: Container(
+            constraints: const BoxConstraints(minWidth: 10, maxWidth: 200),
+            child: Text(
+              message,
+              style: TextStyle(
+                height: 1.5,
+                color: isMe ? Colors.white : Colors.black,
+              ),
+              softWrap: true,
             ),
           ),
         ),
