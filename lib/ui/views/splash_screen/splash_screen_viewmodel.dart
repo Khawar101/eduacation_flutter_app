19d547eefd4c1f7c5ee69780ea8e464f564987ba
khawar101 <khawarjutt101@gmail.com>
2024-02-18 21:40:17 +0500

    s

--- lib/ui/views/splash_screen/splash_screen_viewmodel.dart
+++ lib/ui/views/splash_screen/splash_screen_viewmodel.dart
@@ -1,15 +1,14 @@
-import 'package:education/app/app.router.dart';
 import 'package:education/services/notification_service.dart';
-import 'package:flutter/material.dart';
-import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
-
+import 'package:education/app/app.router.dart';
 import '../../../app/app.locator.dart';
 import 'widgets/SplashContainer1.dart';
 import 'widgets/SplashContainer2.dart';
 import 'widgets/SplashContainer3.dart';
 import 'widgets/SplashContainer4.dart';
 import 'widgets/SplashContainer5.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
 
 class SplashScreenViewModel extends BaseViewModel {
   final _notificationService = locator<NotificationService>();
