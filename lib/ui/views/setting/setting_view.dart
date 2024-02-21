366e9dac05852dea79f08d72b7ee85393d581d2c
khawar101 <khawarjutt101@gmail.com>
2024-02-21 21:44:58 +0500

    s

--- lib/ui/views/setting/setting_view.dart
+++ lib/ui/views/setting/setting_view.dart
@@ -1,8 +1,7 @@
 import 'package:education/ui/views/setting/more_card.dart';
-import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
-
 import '../../widgets/app_utils.dart';
 import 'setting_viewmodel.dart';
 
