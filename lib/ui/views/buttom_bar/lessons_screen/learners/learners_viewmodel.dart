e63c9c60fdc5ffbca9689b906f0cddaf46eb39b2
khawar101 <khawarjutt101@gmail.com>
2024-05-29 19:39:48 +0500

    29

--- lib/ui/views/buttom_bar/lessons_screen/learners/learners_viewmodel.dart
+++ lib/ui/views/buttom_bar/lessons_screen/learners/learners_viewmodel.dart
@@ -1,13 +1,11 @@
-import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
-
 import '../../../../../app/app.locator.dart';
+import 'package:stacked/stacked.dart';
 
 class LearnersViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
 
   navigateonBack() {
-    // log('====>not move');
     _navigationService.back();
   }
 }
